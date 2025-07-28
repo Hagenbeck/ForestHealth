59992bf824b1d423218a54e3584bc5b14294d55a
Hagenbeck <peerschlieker@gmail.com>
2025-07-28 12:14:02 +0200

    style(utils): :bulb: Added comments to date_helper and sentinelhub_api

--- src/utils/date_helper.py
+++ src/utils/date_helper.py
@@ -2,7 +2,9 @@ from datetime import datetime
 import pandas as pd
 
 def parse_date(date: str):
+    '''Converts a valid date string into a datetime object'''
     return datetime.now() if date == "now" else datetime.strptime(date, "%Y-%m-%d")
 
 def generate_monthly_interval(start_date: datetime, end_date: datetime):
+    '''Returns two date_ranges that represent the start and end of the months in the specified interval'''
     return pd.date_range(start=start_date, end=end_date, freq="MS"), pd.date_range(start=start_date, end=end_date, freq="ME")
--- src/utils/sentinelhub_api.py
+++ src/utils/sentinelhub_api.py
@@ -16,6 +16,36 @@ def build_json_request(width_px: int,
                        evalscript_type: EvalScriptType = "RGB", 
                        bbox: list[float] | None = None, 
                        geometry: dict | None = None) -> dict:
+    """
+    Builds the JSON Request for a request to the sentinelhub API.
+
+    Parameters
+    ----------
+    width_px: int
+        Width of result in pixels. Maximum 2500
+    height_px: int
+        Height of result in pixels. Maximum 2500
+    start_date: datetime
+        Start date for request
+    end_date: datetime
+        End date for request
+    evalscript_type: EvalScriptType
+        Specifies use case of request. Either "RGB", "ALL" or "INDICES"
+    bbox: list[float] | None
+        Specifiy bounds of request. Gets preffered over geometry
+    geometry: dict | None
+        Specifiy bounds of request
+
+    Returns
+    -------
+    dict
+        Request for the specified parameters in JSON format.
+    """
+    
+    if width_px > 2500:
+        raise ValueError(f"The API allows for a maximum of 2500 pixels. {width_px} is too wide.")
+    elif height_px > 2500:
+        raise ValueError(f"The API allows for a maximum of 2500 pixels. {width_px} is too high.")
     
     evalscript = get_evalscript(evalscript_type)
     responses = get_response_setup(evalscript_type)
@@ -73,6 +103,24 @@ def build_json_request(width_px: int,
     return json_request
 
 def get_tiling_bounds(geometry: dict, resolution: int = 20, dimension: int = 2500) -> np.ndarray:
+    """
+    Calculates the tiles needed to fetch data from the sentinelhub API at the highest resolution.
+
+    Parameters
+    ----------
+    geometry : dict
+        Dictionary out of a geojson that represents the AOI
+    resolution : int
+        The resolution that each pixel should have. Standard is 20
+    dimension: int
+        The maximum dimension that is allowed in a request. Standard is 2500
+
+    Returns
+    -------
+    np.ndarray
+        Array with the corners of all tiles
+    """
+    
     geom = shape(geometry)
     project = Transformer.from_crs("EPSG:4326", "EPSG:3857", always_xy=True).transform
     geom_m = transform(project, geom)
