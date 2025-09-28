0ec1f0efa49b0802834a90a2b412e0b9174f6401
Hagenbeck <peerschlieker@gmail.com>
2025-09-28 15:43:32 +0200

    style(project): :art: Added ruff formatter

--- src/config.py
+++ src/config.py
@@ -1,8 +1,8 @@
 from data_models import EvalScriptType
 
 # API Request
-EVALSCRIPT_TYPE: EvalScriptType  = "RGB"
-GEOMETRY_FILE: str = "blackForestPoly.geojson" # file with geometry to specify AOI
-START_DATE: str = "2025-07-01" # date as "YYYY-mm-dd"
-END_DATE: str = "2025-07-31" # "now" or YYYY-mm-dd
+EVALSCRIPT_TYPE: EvalScriptType = "RGB"
+GEOMETRY_FILE: str = "blackForestPoly.geojson"  # file with geometry to specify AOI
+START_DATE: str = "2025-07-01"  # date as "YYYY-mm-dd"
+END_DATE: str = "2025-07-31"  # "now" or YYYY-mm-dd
 COLLECTION_ID: str = "sentinel-2-l2a"
--- src/run.py
+++ src/run.py
@@ -1,6 +1,7 @@
 import numpy as np
+
 from pipeline.download import download_sentinel_data
 
-data  = download_sentinel_data()
+data = download_sentinel_data()
 
 np.save("data2020.npy", data)
